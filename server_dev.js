d011a04a8ed83c31b5ea0d1b03af189c77ff6827
Victor <vitya.ryabkov@gmail.com>
2020-10-19 20:27:59 +0300

    Feature: add server file

---
+++ server_dev.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + "/public"))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html", { root: __dirname })
+})
+
+app.listen(5055, () => {
+    console.log('Server was launched succesfully')
+})
